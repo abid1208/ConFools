8e80032d9de8b5d8a974a61627eae5809a55ef99
abid1208 <abid1208@hotmail.com>
2015-03-12 04:50:40 +0500

    Main page header and side bar

--- index.php
+++ index.php
@@ -14,15 +14,36 @@
                     <li> <a class="header-menu-tab" href="#1"> <img src="include/images/basic/logo.png"/> </a> </li>
                     <li> <a class="header-menu-tab" href="#2"> <i class="fa fa-home"> Home</i>  </a> </li>
                     <li> <a class="header-menu-tab" href="#5"> <i class="fa fa-globe"> Discover</i>  </a> </li>
-                    <li style="margin-left: 12%"> <button value="Ask a Poll" href="#6" type="button" class="" >Ask a Poll </button> </li>
-                    <li style="margin-left: 28%;"> <a class="header-menu-tab" href="#5"> <i class="fa fa-bell-o"></i> </a> </li>
-                    <li style="float: right"> <a class="header-menu-tab" href="#5"> <img src="include/images/basic/profile.png"/> </a> </li>
+                    <li style="margin-left: 15%"> <button value="Ask a Poll" href="#6" type="button" class="" >Ask a Poll </button> </li>
+                    <li style="margin-left: 38%;"> <a class="header-menu-tab" href="#5"> <i class="fa fa-bell-o"></i> </a> </li>
+                    <li style="float: right; padding-top: 2px"> <a class="header-menu-tab" href="#5"> <img style="height: 33px;width: 40px" src="include/images/basic/profile.png"/> </a> </li>
                 </ul>
             </header>
     
     <body>
         <div class="left-sidebar">
             <div class="profile-left">
+                <div style="text-align: center; padding-top: 5%"><img style="height: 75px; width: 90px" src="include/images/basic/profile.png"/>
+                    
+                    <p style="margin-top:-1px">Abid Suleman Ahmed</p>
+                    
+                      
+                </div>
+                <div class="paracenter"> <p> 10 </p> </div> <div class="paracenter"> <p> 10 </p> </div> <div class="paracenter"> <p> 10 </p> </div>
+                <div style="margin-top: -10%">
+                <div class="paracenter"> <p> Comments </p> </div> <div class="paracenter"> <p> Following </p> </div> <div class="paracenter"> <p> Followers </p> </div> 
+                </div>
+                </div>
+            
+            <div id="trending">
+                <h3 style="background-color: #CBEDE2"> Trending </h3>
+                <ul>
+                    <li> <a href="#2"> World Cup </a> </li>
+                    <br>
+                    <li> <a href="#3"> Startups </a> </li>
+                    <br>
+                    <li> <a href="#4"> Events </a> </li>
+                </ul>
             
             </div>
         
